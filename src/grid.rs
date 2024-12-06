5d622176798364bc22f31d74865975f9d8f1757c
silentvoid13 <51264226+SilentVoid13@users.noreply.github.com>
2024-12-06 19:00:55 +0100

    feat: adding point

--- src/grid.rs
+++ src/grid.rs
@@ -1,4 +1,6 @@
-use std::ops::{Add, AddAssign, Index, IndexMut, Sub, SubAssign};
+use std::ops::{Index, IndexMut};
+
+use crate::point::Point;
 
 pub trait GridVal: Copy + PartialEq {}
 impl<T> GridVal for T where T: Copy + PartialEq {}
@@ -59,81 +61,5 @@ impl<T: GridVal> IndexMut<Point> for Grid<T> {
     #[inline]
     fn index_mut(&mut self, index: Point) -> &mut Self::Output {
         &mut self.bytes[(index.y * self.width as i32 + index.x) as usize]
-    }
-}
-
-const UP: Point = Point::new(0, -1);
-const LEFT: Point = Point::new(-1, 0);
-const DOWN: Point = Point::new(0, 1);
-const RIGHT: Point = Point::new(1, 0);
-const ORTHO: [Point; 4] = [UP, LEFT, DOWN, RIGHT];
-const DIAG: [Point; 8] = [
-    UP,
-    Point::new(1, -1),
-    LEFT,
-    Point::new(-1, -1),
-    DOWN,
-    Point::new(1, 1),
-    RIGHT,
-    Point::new(-1, 1),
-];
-
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
-pub struct Point {
-    pub x: i32,
-    pub y: i32,
-}
-
-impl Point {
-    #[inline]
-    #[must_use]
-    pub const fn new(x: i32, y: i32) -> Point {
-        Point { x, y }
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn clockwise(self) -> Self {
-        Point::new(-self.y, self.x)
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn counter_clockwise(self) -> Self {
-        Point::new(self.y, -self.x)
-    }
-}
-
-impl Add for Point {
-    type Output = Self;
-
-    #[inline]
-    #[must_use]
-    fn add(self, rhs: Self) -> Self {
-        Point::new(self.x + rhs.x, self.y + rhs.y)
-    }
-}
-
-impl AddAssign for Point {
-    #[inline]
-    fn add_assign(&mut self, rhs: Self) {
-        *self = *self + rhs;
-    }
-}
-
-impl Sub for Point {
-    type Output = Self;
-
-    #[inline]
-    #[must_use]
-    fn sub(self, rhs: Self) -> Self {
-        Point::new(self.x - rhs.x, self.y - rhs.y)
-    }
-}
-
-impl SubAssign for Point {
-    #[inline]
-    fn sub_assign(&mut self, rhs: Self) {
-        *self = *self - rhs;
     }
 }
---
+++ src/point.rs
@@ -0,0 +1,87 @@
+use std::ops::{Add, AddAssign, Mul, Sub, SubAssign};
+
+pub const UP: Point = Point::new(0, -1);
+pub const LEFT: Point = Point::new(-1, 0);
+pub const DOWN: Point = Point::new(0, 1);
+pub const RIGHT: Point = Point::new(1, 0);
+pub const ORTHO: [Point; 4] = [UP, LEFT, DOWN, RIGHT];
+pub const DIAG: [Point; 8] = [
+    UP,
+    Point::new(1, -1),
+    LEFT,
+    Point::new(-1, -1),
+    DOWN,
+    Point::new(1, 1),
+    RIGHT,
+    Point::new(-1, 1),
+];
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Point {
+    #[inline]
+    #[must_use]
+    pub const fn new(x: i32, y: i32) -> Point {
+        Point { x, y }
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn clockwise(self) -> Self {
+        Point::new(-self.y, self.x)
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn counter_clockwise(self) -> Self {
+        Point::new(self.y, -self.x)
+    }
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    #[inline]
+    #[must_use]
+    fn add(self, rhs: Self) -> Self {
+        Point::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl AddAssign for Point {
+    #[inline]
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl Sub for Point {
+    type Output = Self;
+
+    #[inline]
+    #[must_use]
+    fn sub(self, rhs: Self) -> Self {
+        Point::new(self.x - rhs.x, self.y - rhs.y)
+    }
+}
+
+impl SubAssign for Point {
+    #[inline]
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
+impl Mul<i32> for Point {
+    type Output = Self;
+
+    #[inline]
+    #[must_use]
+    fn mul(self, rhs: i32) -> Self {
+        Point::new(self.x * rhs, self.y * rhs)
+    }
+}
